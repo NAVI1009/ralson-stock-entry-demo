1a0effa46a810c13ac13dcd732cf75278a8ee006
NAVI1009 <navdeep02005@gmail.com>
2026-06-21 19:48:58 +0530

    Add files via upload

---
+++ google_sheets.py
@@ -0,0 +1,15 @@
+import streamlit as st
+import gspread
+from google.oauth2.service_account import Credentials
+
+scope = [
+    "https://www.googleapis.com/auth/spreadsheets",
+    "https://www.googleapis.com/auth/drive"
+]
+
+creds = Credentials.from_service_account_info(
+    st.secrets["gcp_service_account"],
+    scopes=scope
+)
+
+client = gspread.authorize(creds)
