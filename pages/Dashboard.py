85fd3377af416aa8d0ddc8c144a270999ea890fa
NAVI1009 <navdeep02005@gmail.com>
2026-06-23 12:03:46 +0530

    Update Dashboard.py

--- pages/Dashboard.py
+++ pages/Dashboard.py
@@ -204,12 +204,10 @@ new_stock = st.number_input(
     value=current_stock,
     min_value=0
 )
-    if pd.isna(current_stock):
-        current_stock = 0
-
-    col1, col2 = st.columns(2)
-
-    with col1:
+if pd.isna(current_stock):
+	current_stock = 0
+	col1, col2 = st.columns(2)
+	with col1:
 
         st.markdown(
             f"""
@@ -227,8 +225,7 @@ new_stock = st.number_input(
             """,
             unsafe_allow_html=True
         )
-
-    with col2:
+        with col2:
         st.markdown(
             f"""
             <div style="
