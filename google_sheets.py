21a578f7459f6ef29e3f8a5af909df252b64fb54
NAVI1009 <navdeep02005@gmail.com>
2026-06-28 19:53:47 +0530

    Update google_sheets.py

--- google_sheets.py
+++ google_sheets.py
@@ -2,14 +2,18 @@ import streamlit as st
 import gspread
 from google.oauth2.service_account import Credentials
 
-scope = [
+# ==========================================
+# GOOGLE SHEETS AUTHENTICATION
+# ==========================================
+
+SCOPES = [
     "https://www.googleapis.com/auth/spreadsheets",
     "https://www.googleapis.com/auth/drive"
 ]
 
-creds = Credentials.from_service_account_info(
+credentials = Credentials.from_service_account_info(
     st.secrets["gcp_service_account"],
-    scopes=scope
+    scopes=SCOPES
 )
 
-client = gspread.authorize(creds)
+client = gspread.authorize(credentials)
