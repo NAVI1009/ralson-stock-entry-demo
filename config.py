f98c4ccc620e7d6df497fb3373e7d19e5dc1e7a8
NAVI1009 <navdeep02005@gmail.com>
2026-06-28 19:54:33 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,1 +1,22 @@
+# ==========================================
+# GOOGLE SHEET CONFIGURATION
+# ==========================================
+
 SHEET_ID = "1Y1l6tF9XfL6GIEPD-Eh4NKUFiVaV_3bccSK-oB47DTQ"
+
+# ==========================================
+# WORKSHEETS
+# ==========================================
+
+MASTER_SHEET = "Master"
+USERS_SHEET = "users"
+LOGS_SHEET = "logs"
+
+# ==========================================
+# MASTER SHEET COLUMNS
+# ==========================================
+
+CODE_COLUMN = "Code"
+MATERIAL_COLUMN = "Material Description"
+STOCK_COLUMN = "13 GT"
+TROLLEY_COLUMN = "Trolley No."
